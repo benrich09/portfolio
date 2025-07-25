1ddafce33911bd37e84267997824843c70f9c41c
BEN RICH <benrich205@gmail.com>
2025-07-25 20:58:12 +0300

    third commit

---
+++ src/Components/sections/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return <section
+        id="home"
+        className="min-h-screen flex items-center justify-center relative ">
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 leading-right">
+                Hi, I am Ben Rich</h1>
+        </div>
+    </section>
+};
