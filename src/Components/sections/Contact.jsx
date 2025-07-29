40913aa6ff0c9e4721954e565ad1db4699c2e7ec
BEN RICH <benrich205@gmail.com>
2025-07-29 08:19:14 +0300

    eleven commit

--- src/Components/sections/Contact.jsx
+++ src/Components/sections/Contact.jsx
@@ -1,11 +1,12 @@
 import {RevealOnScroll} from "../RevealOnScroll.jsx";
 
+
 export const Contact = () => {
 
     return(
         <section
         id="contact"
-        className="min-h-screen flex items-center justify-center py-20">
+        className="min-h-screen flex items-center justify-center relative">
 
             <RevealOnScroll>
                 <div className="px-4 w-150">
